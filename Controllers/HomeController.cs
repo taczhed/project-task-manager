7149f136f166ea492af97a0d4d7641adc6c33d2b
Patryk Lach <patryk.lach@mp.pl>
2024-12-14 11:38:52 +0100

    refactor: basic project configuration

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,11 +18,6 @@ namespace project_task_manager.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
