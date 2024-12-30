7c6b28b776a41e061f7cf1d07756e5208350ec61
Patryk Lach <patryk.lach@mp.pl>
2024-12-30 11:04:37 +0100

    feat: role display

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,22 +1,33 @@
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using project_task_manager.Areas.Identity.Data;
 using project_task_manager.Models;
 using System.Diagnostics;
 
 namespace project_task_manager.Controllers
 {
+
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(UserManager<ApplicationUser> userManager, ApplicationDbContext context)
         {
-            _logger = logger;
+            _userManager = userManager;
+            _context = context;
         }
 
         [Authorize]
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            var user = await _userManager.GetUserAsync(User);
+            var role = _context.UserRoles.First(r => r.ID == user.RoleId);
+            ViewBag.User = user;
+            ViewBag.Role = role;
+
             return View();
         }
 
