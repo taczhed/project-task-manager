645212cf71b88a3bee5954afe8ebb19ef0a532ed
Patryk Lach <patryk.lach@mp.pl>
2024-12-30 11:40:56 +0100

    feat: additional user management

--- Program.cs
+++ Program.cs
@@ -29,11 +29,13 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
-app.MapRazorPages();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
 
 app.Run();
