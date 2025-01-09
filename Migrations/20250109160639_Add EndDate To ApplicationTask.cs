e65c0e548a8043425251e910bf0097f0a786b87c
Patryk Lach <patryk.lach@mp.pl>
2025-01-09 17:08:02 +0100

    fix: EndDate migration

---
+++ Migrations/20250109160639_Add EndDate To ApplicationTask.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace project_task_manager.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEndDateToApplicationTask : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndDate",
+                schema: "Identity",
+                table: "Tasks",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndDate",
+                schema: "Identity",
+                table: "Tasks");
+        }
+    }
+}
